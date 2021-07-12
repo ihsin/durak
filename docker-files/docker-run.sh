54b13399f82c50905825216a8d47da571fd4c402
Rahul Ranjan Sinha <luharihsin@gmail.com>
2021-07-12 21:27:44 +0530

    corrected comment

--- docker-files/docker-run.sh
+++ docker-files/docker-run.sh
@@ -3,4 +3,4 @@ CONTAINER_NAME="durak""$1"
 
 docker run -d --name ${CONTAINER_NAME} -p 8080:5000 durak:latest
 
-[[ $? == 0 ]] && echo "\nCreated Python3 container with name ${CONTAINER_NAME}"
+[[ $? == 0 ]] && echo "\nCreated container with name ${CONTAINER_NAME}"
